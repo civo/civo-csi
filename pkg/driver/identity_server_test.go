8e41d14024d2522b582d2825e8902d257eca85fd
Harsh Thakur <harshthakur9030@gmail.com>
2024-04-16 01:06:23 +0530

    implement health check endpoint
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

---
+++ pkg/driver/identity_server_test.go
@@ -0,0 +1,30 @@
+package driver_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/civo/civo-csi/pkg/driver"
+	"github.com/civo/civogo"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProbe(t *testing.T) {
+	fc, _ := civogo.NewFakeClient()
+	d, _ := driver.NewTestDriver(fc)
+
+	resp, err := d.Probe(context.Background(), &csi.ProbeRequest{})
+	assert.Nil(t, err)
+
+	assert.Equal(t, &wrappers.BoolValue{Value: true}, resp.Ready)
+}
+
+func TestProbeUnhealthy(t *testing.T) {
+	fc, _ := civogo.NewFakeClient()
+	d, _ := driver.NewTestDriver(fc)
+
+	_, err := d.Probe(context.Background(), &csi.ProbeRequest{})
+	assert.NotNil(t, err)
+}
