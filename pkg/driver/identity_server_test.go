33d45a10bf55fbad580dceb35e8a23792688bcad
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2026-07-07 17:14:26 +0900

    feat(identity): advertise online volume expansion capability
    Declare VolumeExpansion_ONLINE alongside OFFLINE in GetPluginCapabilities,
    so the external-resizer permits expanding PVCs while they are in use.
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- pkg/driver/identity_server_test.go
+++ pkg/driver/identity_server_test.go
@@ -30,3 +30,20 @@ func TestProbeUnhealthy(t *testing.T) {
 	_, err := d.Probe(context.Background(), &csi.ProbeRequest{})
 	assert.NotNil(t, err)
 }
+
+func TestGetPluginCapabilities(t *testing.T) {
+	fc, _ := civogo.NewFakeClient()
+	d, _ := driver.NewTestDriver(fc)
+
+	resp, err := d.GetPluginCapabilities(context.Background(), &csi.GetPluginCapabilitiesRequest{})
+	assert.Nil(t, err)
+
+	expansionTypes := []csi.PluginCapability_VolumeExpansion_Type{}
+	for _, capability := range resp.Capabilities {
+		if expansion := capability.GetVolumeExpansion(); expansion != nil {
+			expansionTypes = append(expansionTypes, expansion.Type)
+		}
+	}
+	assert.Contains(t, expansionTypes, csi.PluginCapability_VolumeExpansion_OFFLINE)
+	assert.Contains(t, expansionTypes, csi.PluginCapability_VolumeExpansion_ONLINE)
+}
